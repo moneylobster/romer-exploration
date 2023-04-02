ecb177cd3e4ea45989f0db0da2ff23f47d53e2c8
moneylobster <87999452+moneylobster@users.noreply.github.com>
2023-04-02 16:27:02 +0300

    import motors, init movement

--- controllers/exploration_ctrl/exploration_ctrl.py
+++ controllers/exploration_ctrl/exploration_ctrl.py
@@ -1,8 +1,10 @@
 """exploration_ctrl controller."""
 
-# You may need to import some classes of the controller module. Ex:
-#  from controller import Robot, Motor, DistanceSensor
 from controller import Robot
+import sys
+
+sys.path.append("..")
+from utils.movement import Movement
 
 # create the Robot instance.
 robot = Robot()
@@ -10,23 +12,15 @@ robot = Robot()
 # get the time step of the current world.
 timestep = int(robot.getBasicTimeStep())
 
-# You should insert a getDevice-like function in order to get the
-# instance of a device of the robot. Something like:
-#  motor = robot.getDevice('motorname')
-#  ds = robot.getDevice('dsname')
-#  ds.enable(timestep)
+# get motors
+lm = robot.getDevice("left wheel")
+rm = robot.getDevice("right wheel")
+
+move=Movement(lm, rm)
 
 # Main loop:
 # - perform simulation steps until Webots is stopping the controller
 while robot.step(timestep) != -1:
-    # Read the sensors:
-    # Enter here functions to read sensor data, like:
-    #  val = ds.getValue()
-
-    # Process sensor data here.
-
-    # Enter here functions to send actuator commands, like:
-    #  motor.setPosition(10.0)
     pass
 
-# Enter here exit cleanup code.
+# Exit & cleanup code.
