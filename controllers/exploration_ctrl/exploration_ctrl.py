086b9e8bb3158af6d2ad8cc4224ff80c95f603fe
usermech <umutkrt98@gmail.com>
2023-04-25 18:28:24 +0300

    finds marker

--- controllers/exploration_ctrl/exploration_ctrl.py
+++ controllers/exploration_ctrl/exploration_ctrl.py
@@ -25,16 +25,36 @@ rm = robot.getDevice("right wheel")
 camera = robot.getDevice("camera")
 camera.enable(timestep)
 
+front_cam = robot.getDevice("front camera")
+front_cam.enable(timestep)
+
 move = Movement(lm, rm)
 
 # points=[[2,1,-1],
 #         [0,0,1],
 #         [1,1,0]]
 #points=[[0,0]]
-
+def check_aruco(img,pose,aruco_dict=cv2.aruco.DICT_4X4_100):
+    cameraMatrix = np.array([[528, 0, 360],[0, 528, 360],[0, 0, 1]])
+    distCoeffs = np.array([0, 0, 0, 0, 0])
+    aruco_dict = cv2.aruco.Dictionary_get(aruco_dict)
+    parameters =  cv2.aruco.DetectorParameters_create()
+    corners, ids, rejectedImgPoints = cv2.aruco.detectMarkers(img, aruco_dict, parameters=parameters)
+    if ids is not None:
+        '''
+        rvec, tvec ,_ = cv2.aruco.estimatePoseSingleMarkers(corners, 0.05, cameraMatrix, distCoeffs)
+        robotx,roboty,robottheta=pose
+        marker_x = robot_x + tvec[0][0][0]*np.cos(robottheta) - tvec[0][0][2]*np.sin(robottheta)
+        marker_y = robot_y + tvec[0][0][0]*np.sin(robottheta) + tvec[0][0][2]*np.cos(robottheta)
+        m
+        '''
+        return True, ids[0][0]
+    else:
+        return False, None
 
 i=0
-
+MARKER_NUM = 4
+seen_markers = []
 #move.linmoveto(*points[0])
 
 # Main loop:
@@ -45,7 +65,18 @@ while robot.step(timestep) != -1:
     img = np.frombuffer(img, np.uint8).reshape(camera.height, camera.width, 4)
     img = cv2.cvtColor(img, cv2.COLOR_BGRA2RGB)
 
+    front_img = front_cam.getImage()
+    front_img = np.frombuffer(front_img, np.uint8).reshape(front_cam.height, front_cam.width, 4)
+    front_img = cv2.cvtColor(front_img, cv2.COLOR_BGRA2RGB) 
+
     state = posest(img, TAG_METHOD) 
+
+    flag, id = check_aruco(front_img,state)
+    if flag:
+        if id not in seen_markers:
+            seen_markers.append(id)
+        if len(seen_markers) == MARKER_NUM:
+            print("All markers seen")
 
     if i==0:
         stats, path=pathplan(tuple(state[:2]))
